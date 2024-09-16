5bdea1763cc7d209e6e10d9afd493876ed2f56fd
washington-shoji <shoji.washington@gmail.com>
2024-09-17 07:01:37 +1000

    - added tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: ["./templates/**/*.templ"],
+    darkMode: 'class',
+    theme: {
+      extend: {
+        fontFamily: {
+          mono: ['Courier Prime', 'monospace'],
+        }
+      },
+    },
+    plugins: [
+      require('@tailwindcss/forms'),
+    ],
+    corePlugins: {
+      preflight: true,
+    }
+  }
+  
