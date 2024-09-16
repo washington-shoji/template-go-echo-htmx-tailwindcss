f9b1e3b75dec340bb00e0d70f6219309bb98ff69
washington-shoji <shoji.washington@gmail.com>
2024-09-17 07:00:53 +1000

    - updated main.go

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main()  {
-	
+import (
+	"personal-portfolio/routes"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	// Serve static files
+	e.Static("/css", "public/css")
+	e.Static("/js", "assets/js")
+
+	// Register routes
+	routes.Register(e)
+
+	// Start the server
+	e.Logger.Fatal(e.Start(":8080"))
 }
